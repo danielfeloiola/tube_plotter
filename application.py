d8dc7bfbf19a14461af769cb53979c74d93729d4
Daniel Loiola <danf10@me.com>
2021-02-09 23:38:05 -0300

    testing more changes to session

--- application.py
+++ application.py
@@ -30,7 +30,7 @@ app.config['SECRET_KEY'] = ''.join(random.choice(string.ascii_letters) for i in 
 #app.config['SESSION_PERMANENT'] = False
 #app.config['SESSION_TYPE'] = 'filesystem'
 #app.config['SESSION_COOKIE_SECURE'] = True
-#Session(app)
+Session(app)
 
 # Ensure responses aren't cached
 @app.after_request
